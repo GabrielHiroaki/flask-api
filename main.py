48ddbe5ae1882706a58e0103d7a2a9b451dc6bcc
GabrielHiroaki <103044026+GabrielHiroaki@users.noreply.github.com>
2023-11-01 14:47:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,4 +238,4 @@ def page_not_found(e):
     return jsonify({"status": response.status_code, "mensagem": response.text})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
